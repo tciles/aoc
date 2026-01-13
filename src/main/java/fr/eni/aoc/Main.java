6bff5b6aad1e1d98d832f127d075a8abe8427de9
Thomas CILES <thomas.ciles2025@campus-eni.fr>
2026-01-13 16:51:08 +0100

    day 8

--- src/main/java/fr/eni/aoc/Main.java
+++ src/main/java/fr/eni/aoc/Main.java
@@ -5,7 +5,7 @@ import fr.eni.aoc.aoc2015.*;
 public class Main {
     public static void main(String[] args) {
         try {
-            new Day07().run();
+            new Day08().run();
         } catch (Exception e) {
             System.err.println(e);
         }
---
+++ src/main/java/fr/eni/aoc/aoc2015/Day08.java
@@ -0,0 +1,39 @@
+package fr.eni.aoc.aoc2015;
+
+import fr.eni.aoc.BaseDay;
+
+import java.io.BufferedReader;
+import java.util.*;
+
+public class Day08 extends BaseDay {
+
+    public Day08() {
+        day = "08";
+    }
+
+    @Override
+    protected void answerOne() throws Exception {
+        BufferedReader reader = getInputContent();
+
+        String line;
+        while ((line = reader.readLine()) != null) {
+            System.out.println(line);
+        }
+    }
+
+    @Override
+    protected void answerTwo() throws Exception {
+        /*
+        BufferedReader reader = getInputContent();
+
+        String line;
+        while ((line = reader.readLine()) != null) {
+            System.out.println(line);
+        }
+
+        int total = 0;
+
+        System.out.println("Answer 2 : " + total);
+        */
+    }
+}
